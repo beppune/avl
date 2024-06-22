01b46fa86df168f37ffb83c7864af802ff467a21
beppune <manzo.giuseppe@gmail.com>
2024-06-22 22:36:29 +0200

    silly print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::mem;
+use std::cmp;
 
 enum Tree {
     Empty,
@@ -14,11 +16,53 @@ impl Tree {
 
     pub fn new() -> Tree { Tree::Empty }
 
+    pub fn put(&mut self, elem:i32) -> &mut Self {
+        match self {
+            Tree::Empty => {
+                let n = Node { elem, right: Tree::Empty, left: Tree::Empty };
+                let _ = mem::replace( self, Tree::More(Box::new(n)) );
+            },
+            Tree::More(n) => {
+
+                if elem > n.elem {
+                    n.right.put(elem);
+                }
+                else if elem < n.elem {
+                    n.left.put(elem);
+                }
+
+            }
+        }
+        self
+    }
+
+    pub fn print(&self) {
+        match self {
+            Tree::Empty => {
+                //do nothing
+            },
+            Tree::More(n) => {
+                println!("{: >width$}", n.elem, width=self.height()*2 );
+                n.left.print();
+                n.right.print();
+            }
+        }
+    }
+
+    fn height(&self) -> usize {
+        match self {
+            Tree::More(n) => {
+                return 1 + cmp::max( n.left.height(), n.right.height() );
+            },
+            Tree::Empty => 0,
+        }
+    }
+
 }
 
 
 fn main() {
     let mut tree = Tree::new();
-
-
+    tree.put(34).put(12).put(40);
+    tree.print();
 }
