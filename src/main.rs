d34a48818d774a6b9f866ffeba34abbab124a0d2
beppe <manzo.giuseppe@gmail.com>
2024-07-05 18:25:15 +0200

    Tree

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+struct Node {
+    value: i32,
+    left: SubTree,
+    right: SubTree,
+}
+
+type SubTree = Option<Box<Node>>;
+
+struct Dict {
+    root: SubTree,
+}
+
+impl Dict {
+    fn new() -> Self {
+        Dict { root: None }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
