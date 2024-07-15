2d5769f26305afd5b1ff73aed874752e0ea9fd49
beppune <manzo.giuseppe@gmail.com>
2024-07-15 10:57:24 +0200

    extrapolated h funxtion

--- src/lib.rs
+++ src/lib.rs
@@ -15,45 +15,52 @@ struct Node {
     left: SubTree,
 }
 
+enum Ballance{
+    Ballanced,
+    LeftHeavy,
+    RightHeavy,
+}
+
 impl Tree {
 
     pub fn new() -> Self {
         Tree{ root: None, size: 0 }
     }
-    
+
     pub fn put(&mut self, value:i32) -> bool {
         let mut current = &mut self.root;
         while let Some(box_node) = current {
-            
+
             if box_node.value < value { current = &mut box_node.right; }
             else if box_node.value > value { current = &mut box_node.left; }
             else { return false; }
-            
+
         }
-        
+
         *current = Some(Box::new(Node{
             value,
             right:None,
             left:None,
         }));
-        
+
         self.size += 1;
-    
+
         true
     }
 
     pub fn height(&mut self) -> usize {
 
-        fn h(sub:&SubTree) -> usize {
-            match sub {
-                Some(boxed) => 1 + cmp::max( h(&boxed.left), h(&boxed.right) ),
-                None => 0,
-            }
-        }
-
-        h(&self.root)
+        Self::h(&self.root)
 
     }
+
+    fn h(sub:&SubTree) -> usize {
+        match sub {
+            Some(boxed) => 1 + cmp::max( Self::h(&boxed.left), Self::h(&boxed.right) ),
+            None => 0,
+        }
+    }
+
 
 }
 
@@ -64,7 +71,7 @@ mod test {
 
     #[test]
     fn test1() {
-        
+
         let mut tree = Tree::new();
         tree.put(12);
         tree.put(15);
@@ -72,7 +79,7 @@ mod test {
         println!("Height: {}", tree.height());
         assert!( tree.height() == 2 );
         dbg!(tree.root);
-        
+
     }
-    
+
 }
