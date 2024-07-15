4ecbe11cccb74af02b4ca4f09bd2d1a7ac3543ed
beppune <manzo.giuseppe@gmail.com>
2024-07-15 14:13:05 +0200

    rotate function

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,9 @@ struct Node {
 enum Ballance{
     Ballanced,
     LeftHeavy,
+    //LeftRightHeavy,
     RightHeavy,
+    //RightLeftHeavy,
 }
 
 impl Tree {
@@ -61,6 +63,33 @@ impl Tree {
         }
     }
 
+    fn bf(sub:&SubTree) -> Ballance {
+
+        match sub {
+            Some(boxed) => {
+                let b = Self::h(&boxed.left) as i32 - Self::h(&boxed.right) as i32;
+                match b {
+                    0 => Ballance::Ballanced,
+                    _ if b > 0 => Ballance::LeftHeavy,
+                    _ if b < 0 => Ballance::RightHeavy,
+                    _ => panic!("ballance value: {}", b),
+                }
+            }
+            None => Ballance::Ballanced,
+        }
+
+    }
+
+    fn rotate(sub:&SubTree) {
+
+        let b = Self::bf(sub);
+        match b {
+            Ballance::LeftHeavy => println!("LeftHeavy"),
+            Ballance::RightHeavy => println!("RightHeavy"),
+            Ballance::Ballanced => println!("Ballanced"),
+        }
+
+    }
 
 }
 
